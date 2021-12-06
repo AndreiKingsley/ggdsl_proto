cdc4471e582dcb357fd2817547ab398d1395466b
Andrei Kislitsyn <kisandy@yandex.ru>
2021-12-06 15:28:29 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+    `maven-publish`
+}
+
+group = "com.andreikingsley"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.andreikingsley"
+            artifactId = "ggdsl_proto"
+            version = "1.0.1"
+
+            from(components["java"])
+        }
+    }
+}
